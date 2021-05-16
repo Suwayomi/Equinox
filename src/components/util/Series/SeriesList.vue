7f5df8a439fe7dbd53fcdcf2f5dd3eb72d10415a
Jip Fr <jipfrijlink@gmail.com>
2021-05-16 19:10:36 +0200

    Re-style series list

--- src/components/util/Series/SeriesList.vue
+++ src/components/util/Series/SeriesList.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="series-list">
-    <slot />
-  </div>
+	<div class="series-list">
+		<slot />
+	</div>
 </template>
 
 <style lang="scss" scoped>
 @use '../../../index' as v;
 
 .series-list {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
-  grid-gap: 40px 20px;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+	grid-gap: 20px 15px;
 }
 
 @media (max-width: v.$breakpoint) {
-  .series-list {
-    grid-template-columns: 100%;
-    grid-gap: 20px;
-  }
+	.series-list {
+		grid-template-columns: 100%;
+		grid-gap: 20px;
+	}
 }
 </style>
