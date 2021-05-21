d266e6ba78bef9240608b435d0880f23d781cffd
Jip Fr <jipfrijlink@gmail.com>
2021-05-21 17:47:11 +0200

    feat(components): tag component

---
+++ src/components/util/Buttons/Tag.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="badge">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.badge {
+	display: inline-block;
+	padding: 2px 8px;
+	border-radius: 4px;
+	background: var(--theme);
+	color: var(--text-on-theme);
+}
+</style>
